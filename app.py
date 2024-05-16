a66cfeb40697ebe0471485d12c1e5c783f130d48
admingeves <gustavo@geves.cl>
2024-05-15 20:07:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -71,8 +71,8 @@ def main_interface():
 #MENU DEL SIDEBAR
 
         selected = option_menu(menu_title=None, options=['Bodega', 'Costos', 'Seguimiento'], icons=['boxes', 'coin', 'list-task'])
-        p1 = st.selectbox(label='Cliente', options=['INCOPROV'] + ['INCOPROV'], label_visibility='hidden', placeholder='Cliente')
-        p2 = st.selectbox(label='Empresa', options=['HGM'] + ['INCOPROV', 'HGM'], label_visibility='hidden', placeholder='Empresa')
+        p1 = st.selectbox(label='Cliente', options=['INCOPROV'], label_visibility='hidden', placeholder='Cliente')
+        p2 = st.selectbox(label='Empresa', options=['INCOPROV', 'HGM'], label_visibility='hidden', placeholder='Empresa')
         
         
         
@@ -80,18 +80,18 @@ def main_interface():
 
     if selected == 'Bodega':
         st.title('EPP')
-        col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
+        col1, col2, col3, col4 = st.columns([2, 1, 1, 1])
             
         with col1:    
-            mes_inicio,mes_fin=st.select_slider(label='Rango Fecha Bodega', options=list(meses_dict.keys()), value=['Enero','Diciembre'], label_visibility='hidden')
+            mes_inicio,mes_fin=st.select_slider(label='Rango Fecha Bodega', options=list(meses_dict.keys()), value=['Enero','Diciembre'], label_visibility='hidden', )
             fecha_inicio = meses_dict[mes_inicio]
             fecha_fin = meses_dict_fin[mes_fin]
         
-        #with col2:
-        with col3:
+        
+        with col2:
             p4 = st.text_input(label='Mes Inicio API', value=(fecha_inicio), label_visibility='hidden', disabled=True)
         
-        with col4:
+        with col3:
             p5 = st.text_input(label='Mes Fin API', value=(fecha_fin), label_visibility='hidden', disabled=True, )
     
 # LLAMADA APIbodega.py
@@ -126,14 +126,14 @@ def main_interface():
             filtered_data_obra = filtered_data_clase[filtered_data_clase['obra'] == obra_seleccionada] if obra_seleccionada else filtered_data_clase
     
         with col3:
-            recursos = (filtered_data_clase['nombreRecurso'].unique())
+            recursos = (filtered_data_obra['nombreRecurso'].unique())
             recurso_seleccionado = st.selectbox(label='Recurso', options=[''] + list(recursos), placeholder='Recursos')
             filtered_data_recurso = filtered_data_obra[filtered_data_obra['nombreRecurso'] == recurso_seleccionado] if recurso_seleccionado else filtered_data_obra
     
         with col4:
-            recibe = (filtered_data_clase['recibe'].unique())
+            recibe = (filtered_data_recurso['recibe'].unique())
             trabajador_seleccionado = st.selectbox(label='Trabajador', options=[''] + list(recibe), placeholder='Nombre Trabajador')
-            filtered_data_trabajador = filtered_data_obra[filtered_data_obra['recibe'] == trabajador_seleccionado] if trabajador_seleccionado else filtered_data_recurso
+            filtered_data_trabajador = filtered_data_recurso[filtered_data_recurso['recibe'] == trabajador_seleccionado] if trabajador_seleccionado else filtered_data_recurso
     
     #MENU RESUMEN, FLUJO ECONOMICO, USO RECURSO
         
@@ -142,23 +142,32 @@ def main_interface():
         #CLICK MENU CANTIDAD, MONTO, DATOS
         if selected == 'Cantidad':
             #TOTAL CANTIDAD
+            data_bodega=pd.DataFrame(filtered_data_trabajador)
+            suma_data_bodega=data_bodega.groupby('fecha')['cantidad'].sum().reset_index()
+            suma_obra_bodega=data_bodega.groupby('obra')['cantidad'].sum().reset_index()
+            suma_recibe_bodega=data_bodega.groupby('recibe')['cantidad'].sum().reset_index()
+            suma_recurso_bodega=data_bodega.groupby('nombreRecurso')['cantidad'].sum().reset_index()
+            
+
+
 
             total_cantidad = int(filtered_data_trabajador['cantidad'].sum())
+        
             st.metric(label='Total Cantidad', value=total_cantidad)
-    
+        
             #GRAFICO CANTIDAD 
-               
-            graficoCantidad = st.bar_chart(filtered_data_trabajador, x='fecha', y='cantidad', width=0, height=0, use_container_width=True)
+            graficoCantidad=st.bar_chart(suma_data_bodega.set_index('fecha'))
+            #graficoCantidad = st.bar_chart(filtered_data_trabajador, x='fecha', y='cantidad', width=0, height=0, use_container_width=True)
     
             #GRAFICOS OBRA, RECURSO, RECIBE
 
             col1, col2, col3 = st.columns([1, 1, 1])
             with col1:
-                graficoObra = st.bar_chart(filtered_data_trabajador, x='obra', y='cantidad', width=0, height=0, use_container_width=True)
+                graficoObra = st.bar_chart(suma_obra_bodega.set_index('obra'))
             with col2:
-                graficoRecibe = st.bar_chart(filtered_data_trabajador, x='recibe', y='cantidad', width=0, height=0, use_container_width=True)
+                graficoRecibe = st.bar_chart(suma_recibe_bodega.set_index('recibe'))
             with col3:
-                graficoRecurso = st.bar_chart(filtered_data_trabajador, x='nombreRecurso', y='cantidad', width=0, height=0, use_container_width=True)
+                graficoRecurso = st.bar_chart(suma_recurso_bodega.set_index('nombreRecurso'))
     
         if selected == 'Monto':
             #TOTAL MONTO
@@ -203,32 +212,48 @@ def main_interface():
         filtered_obra = [{column: entry[column] for column in columns_obra} for entry in datos_obra]
         filtered_data_obra = pd.DataFrame(filtered_obra)
         
-        
         st.title('Costos')
-        col1,col2,col3,col4= st.columns([1,1,1,1])
+        col1,col2= st.columns([1,3])
         
         with col1:
-            par3=st.selectbox(label='Año', options=['2024','2023'])
-        filtro_obra = (filtered_data_obra['codObra'].unique())
-
-        with col2:
-            par4=st.selectbox(label='Obra', options=[''] + list(filtro_obra))
-
+            par3=st.selectbox(label='Año', options=['2024','2023'], label_visibility='hidden')
+            filtro_obra = (filtered_data_obra['codObra'].unique())
+            par4=st.selectbox(label='Obra', options=[''] + list(filtro_obra), label_visibility='hidden')
 
 #LLAMADA APIconsumos
         
-        APIconsumos = response_consumo(p1, p2, par3, par4)
-        datos = APIconsumos.json()['datos']
+            APIconsumos = response_consumo(p1, p2, par3, par4)
+            datos = APIconsumos.json()['datos']
         # Columnas que voy a llamar de 'datos'
-        columns = ['obra', 'cantidad']
+            columns = ['obra', 'cantidad', 'tipoCosto', 'codigoArea', 'nombrePartida', 'nombreRecurso', 'fecha']
         # Armar un nuevo DF para mostrar las columnas seleccionadas
-        filtered = [{column: entry[column] for column in columns} for entry in datos]
+            filtered = [{column: entry[column] for column in columns} for entry in datos]
 
-        filtered_data_consumos = pd.DataFrame(filtered)
-        st.table(filtered_data_consumos)
+            filtered_data_consumos = pd.DataFrame(filtered)
+
+            t_costo=(filtered_data_consumos['tipoCosto'].unique())
+            t_costo_seleccionado=st.selectbox(label='Tipo Costo', options=[''] + list(t_costo), placeholder='Tipo de Costos', label_visibility='hidden' )
+            filtered_t_costo = filtered_data_consumos[filtered_data_consumos['tipoCosto'] == t_costo_seleccionado] if t_costo_seleccionado else filtered_data_consumos
+
+            area_consumo= (filtered_t_costo['codigoArea'].unique())
+            area_consumo_seleccionada=st.selectbox(label='Área', options=[''] + list(area_consumo), placeholder='Nombre Área', label_visibility='hidden')
+            filtered_area_consumo = filtered_t_costo[filtered_t_costo['codigoArea'] == area_consumo_seleccionada] if area_consumo_seleccionada else filtered_t_costo
+
+            partida_consumo= (filtered_area_consumo['nombrePartida'].unique())
+            partida_consumo_seleccionada=st.selectbox(label='Partida', options=[''] + list(partida_consumo), placeholder='Nombre Partida', label_visibility='hidden')
+            filtered_partida_consumo = filtered_area_consumo[filtered_area_consumo['nombrePartida'] == partida_consumo_seleccionada] if partida_consumo_seleccionada else filtered_area_consumo
+
+            recurso_consumo= (filtered_partida_consumo['nombreRecurso'].unique())
+            recurso_consumo_seleccionada=st.selectbox(label='Recurso', options=[''] + list(recurso_consumo), placeholder='Nombre Recurso', label_visibility='hidden')
+            filtered_recurso_consumo = filtered_partida_consumo[filtered_partida_consumo['nombreRecurso'] == recurso_consumo_seleccionada] if recurso_consumo_seleccionada else filtered_partida_consumo
+
+        with col2:
+            with st.container(height=400):
+                data_consumo=pd.DataFrame(filtered_recurso_consumo)
+                suma_data_consumo=data_consumo.groupby('fecha')['cantidad'].sum().reset_index()
+                st.bar_chart(suma_data_consumo.set_index('fecha'))
         
-
-
+                #st.table(filtered_recurso_consumo)
 
     
     if selected == 'Seguimiento':
